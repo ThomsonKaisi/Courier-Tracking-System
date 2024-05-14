022147fbe5797fb2a4f40556aea92b0dfb022c6a
Thomson Kaisi <thomsonkaisi@gmail.com>
2024-05-14 10:25:37 +0200

    Chang

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey,DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-import Da
 Base = declarative_base()
 
 class User(Base):
